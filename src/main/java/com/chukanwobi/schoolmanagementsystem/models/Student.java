eabd8e07c3e66a7887c85cf1806a3bfafee31f6e
cnwobi <c.nwobi@outlook.com>
2018-08-24 09:48:48 +1000

    tostring() referece to courseconduction from student causes stackoverflow

--- src/main/java/com/chukanwobi/schoolmanagementsystem/models/Student.java
+++ src/main/java/com/chukanwobi/schoolmanagementsystem/models/Student.java
@@ -1,17 +1,17 @@
 package com.chukanwobi.schoolmanagementsystem.models;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Entity
-@Data
+@Getter
+@Setter
 public class Student implements UserDetails {
     @javax.persistence.Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -69,5 +69,19 @@ public class Student implements UserDetails {
     @Override
     public boolean isEnabled() {
         return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "Id=" + Id +
+                ", image=" + Arrays.toString(image) +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", major='" + major + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
